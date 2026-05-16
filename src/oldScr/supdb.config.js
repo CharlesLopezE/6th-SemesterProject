00272fca5bae69f3d774c34767ffe01774cfabc5
Llama <150065016+CharlesLopezE@users.noreply.github.com>
2026-05-16 11:43:41 -0700

    Add login, signup, my trips, and saved schedules pages with Supabase integration
    - Created login.html for user authentication with email and password.
    - Developed signin.html for user registration, including first name and password confirmation.
    - Implemented mytrip&act.html to display user's trips and recent activity.
    - Added savedschedules.html for users to view and manage saved schedules.
    - Introduced supdb.config.js for Supabase client configuration.
    - Enhanced user experience with modals for about, contact, and settings.
    - Included functionality for loading user data and handling logout.

---
+++ src/oldScr/supdb.config.js
@@ -0,0 +1,6 @@
+// supdb.config.js
+
+NEXT_PUBLIC_SUPABASE_URL="https://rnfxcbicrmnshiruzdph.supabase.co"
+NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY="sb_publishable_jPxH_KfS41BKfWYYU0hEDg_zm2aCMqy"
+
+const supabaseClient = supabase.createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
